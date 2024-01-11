369c2ccf93d8efcfce169bd0da1a245f11fa2eef
Tommy Ngo <tommyquocngo@gmail.com>
2024-01-11 14:33:51 -0800

    Final touches

--- Program.cs
+++ Program.cs
@@ -1,3 +1,8 @@
+using MiniChallengeEightToTen.Service;
+using MiniChallengeEightToTen.Service.GuessIt;
+using MiniChallengeEightToTen.Service.Magic8;
+using MiniChallengeEightToTen.Service.RestaurantPicker;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +11,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddScoped<IGuessItService, GuessItService>();
+builder.Services.AddScoped<IRestaurantPickerService, RestaurantPickerService>();
+builder.Services.AddScoped<IMagic8Service, Magic8Service>();
 
 var app = builder.Build();
 
